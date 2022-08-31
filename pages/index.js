b6ce9ede287c6d4a3605fb8c866646fee71e1b7e
Hao Xu <b.hao.xu@gmail.com>
2022-08-31 14:34:49 +1000

    feat: added Tailwindcss to project

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
-import Head from "next/head";
-import { handleGoogleLogin } from "../services/auth";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import { handleGoogleLogin } from '../services/auth';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Recipe App</title>
-        <meta name="description" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <button onClick={handleGoogleLogin}>Sign In</button>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       </main>
     </div>
   );
