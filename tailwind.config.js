d52fd2a9866b49b2b57bc8a91bc686d462ce74b7
Hao Xu <b.hao.xu@gmail.com>
2022-09-01 10:17:06 +1000

    feat: configured tailwind theme according to design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,27 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      lora: ["Lora"],
+      nunito: ["nunito"],
+    },
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      colors: {
+        dirt: "#A06A50",
+        chestnut: "#55433B",
+        cultured: "#F8F7F5",
+        lotion: "#FFFDFA",
+        platinum: "#E3E2E1",
+        rajah: "#FFAC59",
+
+        "blanched-almond": "#FFEFCA",
+        "chrome-yellow": "#FFAC00",
+        "floral-white": "#FFFAF0",
+        "pale-silver": "#C3C0B9",
       },
     },
   },
