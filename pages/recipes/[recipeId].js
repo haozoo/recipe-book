2c9cc15d5ee9ce8932329fdd878ff53720f3f1a0
shuetyi-lee <89139961+shuetyi-lee@users.noreply.github.com>
2022-09-21 12:38:00 +1000

    refactor: updated single recipe's styling, added favorite icon

--- pages/recipes/[recipeId].js
+++ pages/recipes/[recipeId].js
@@ -15,68 +15,83 @@ export default function SingleRecipePage({ recipe }) {
   const ingredients = recipe.ingredients;
   const instructions = recipe.instructions;
 
-    return (
-      <div className="single-recipe pt-10">
-        <div className="flex flex-row justify-between space-x-24">
-          <div className="flex w-full flex-col">
-            <div>
+  const [favourited, setFavourited] = useState(recipe.favourited);
+
+  const handleFavourite = () => {
+    setFavourited(!favourited);
+  };
+
+  return (
+    <div className="single-recipe pt-10">
+      <div className="flex flex-row justify-between space-x-24">
+        <div className="flex w-full flex-col">
+          <div>
+            <div className="flex flex-row justify-between">
               <h1 className="font-architectsDaughter font-bold text-chestnut tracking-wide text-4xl">
                 {recipe.title}
               </h1>
-              <div className="flex flex-row my-6 space-x-4">
-                <div className="flex flex-col">
-                  <p className="text-xs">Prep time</p>
-                  {recipe.prepTime} mins
-                </div>
-                <div className="flex flex-col">
-                  <p className="text-xs">Cook time</p>
-                  {recipe.cookTime} mins
-                </div>
-                <div className="flex flex-col">
-                  <p className="text-xs">Total time</p>
-                  {convertTime(recipe.prepTime + recipe.cookTime)}
-                </div>
+              <button onClick={handleFavourite}>
+                {favourited ? (
+                  <FullHeartIcon className="text-red-400 flex-shrink-0 h-10 w-10" />
+                ) : (
+                  <EmptyHeartIcon className="text-chestnut flex-shrink-0 h-10 w-10" />
+                )}
+              </button>
+            </div>
+            <div className="flex flex-row my-6 space-x-4">
+              <div className="flex flex-col">
+                <p className="text-xs">Prep time</p>
+                {recipe.prepTime} mins
               </div>
-              
-              <div className="py-2 border-y border-solid border-chestnut">
-                <p className="text-xs mb-2">Tags</p>
-                <div className="flex flex-row space-x-4 text-sm">
-                  {tags.map((tag, i) => (
-                    <button key={i} className="odd:bg-chrome-yellow even:bg-dirt hover:bg-yellow text-white px-4 py-2 rounded-full">{tag}</button>
-                  ))}
-                </div>
+              <div className="flex flex-col">
+                <p className="text-xs">Cook time</p>
+                {recipe.cookTime} mins
+              </div>
+              <div className="flex flex-col">
+                <p className="text-xs">Total time</p>
+                {convertTime(recipe.prepTime + recipe.cookTime)}
               </div>
             </div>
-
-            <div>
-              <p className="font-architectsDaughter text-chestnut tracking-wide text-2xl pt-4">Ingredients</p>
-              <ol style={{ listStyleType: 'disc' }}>
-                {ingredients.map((ingredient, i) => (
-                  <li key={i}>{ingredient}</li>
+            
+            <div className="py-2 border-y border-solid border-chestnut">
+              <p className="text-xs mb-2">Tags</p>
+              <div className="flex flex-row space-x-4 text-sm">
+                {tags.map((tag, i) => (
+                  <button key={i} className="odd:bg-chrome-yellow even:bg-dirt hover:bg-yellow text-white px-4 py-2 rounded-full">{tag}</button>
                 ))}
-              </ol>
+              </div>
             </div>
-            <div>
-              <p className="font-architectsDaughter text-chestnut tracking-wide text-2xl pt-4">Instructions</p>
-              <ol style={{ listStyleType: 'decimal' }}>
-                {instructions.map((instruction, i) => (
-                  <li key={i}>{instruction}</li>
-                ))}
-              </ol>
-            </div>
-
           </div>
-          <div className="flex w-full flex-col">
-            <div className="aspect-square h-72 flex items-center justify-center overflow-hidden rounded-lg sm:aspect-w-2 sm:aspect-h-3 bg-blanched-almond">
-              <img src={recipe.imageSrc[0]} alt="" className="h-full"/>
-            </div>
-            <div>
-              <p className="font-architectsDaughter text-chestnut tracking-wide text-2xl pt-4">Gallery</p>
-            </div>
+
+          <div>
+            <p className="font-architectsDaughter text-chestnut tracking-wide text-2xl pt-4">Ingredients</p>
+            <ol style={{ listStyleType: 'disc' }} className="pl-6 font-nunito">
+              {ingredients.map((ingredient, i) => (
+                <li key={i} className="pl-2">{ingredient}</li>
+              ))}
+            </ol>
+          </div>
+          <div>
+            <p className="font-architectsDaughter text-chestnut tracking-wide text-2xl pt-4">Instructions</p>
+            <ol style={{ listStyleType: 'decimal' }} className="pl-6">
+              {instructions.map((instruction, i) => (
+                <li key={i} className="pl-2 font-nunito">{instruction}</li>
+              ))}
+            </ol>
+          </div>
+
+        </div>
+        <div className="flex w-full flex-col">
+          <div className="aspect-square h-72 flex items-center justify-center overflow-hidden rounded-lg sm:aspect-w-2 sm:aspect-h-3 bg-blanched-almond">
+            <img src={recipe.imageSrc[0]} alt="" className="h-full"/>
+          </div>
+          <div>
+            <p className="font-architectsDaughter text-chestnut tracking-wide text-2xl pt-4">Gallery</p>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 SingleRecipePage.getLayout = (page) => {
