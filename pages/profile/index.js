4a1ec832099cad289e238844523555cb61dcac0b
shuetyi-lee <89139961+shuetyi-lee@users.noreply.github.com>
2022-10-12 16:06:57 +1100

    refactor: made minor style updates to loginModal and profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -97,7 +97,7 @@ const UserInfoHeading = ({ heading, text, name, Icon, handleUpload, data }) => {
           })}
         <div className="flex items-center mt-2">
           <button
-            className="py-2 px-4 bg-sajah rounded-lg text-white text-sm font-patrick tracking-wider font-extrabold disabled:opacity-30"
+            className="py-2 px-4 bg-rajah rounded-lg text-white text-sm font-patrick tracking-wider font-extrabold disabled:opacity-30"
             type="button"
             disabled={
               isLoading ||
