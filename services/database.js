7ed9615b603a587769ba2e2fc2ff5aaa737d4841
Hao Xu <b.hao.xu@gmail.com>
2022-10-18 14:34:33 +1100

    refactor: fixed bug loading tag

--- services/database.js
+++ services/database.js
@@ -182,6 +182,8 @@ export const getAllRecipes = async (uid) => {
       const { defaultTags, userAddedTags, ...recipeInfo } = recipe;
       return {
         ...recipeInfo,
+        defaultTags, 
+        userAddedTags,
         allTags: [].concat(defaultTags).concat(userAddedTags),
       };
     });
